a401a9ddb7a322f24c2ab30bbf60d5864fa82d60
korekorokero <koresy.samuel20@gmail.com>
2024-02-16 16:02:15 +0700

    feat: collider and temp dynamic layer sorting

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,25 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using System.Threading;
 using Unity.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Playables;
+using Vector2 = UnityEngine.Vector2;
 
 public class PlayerMovement : MonoBehaviour
 {
     private enum MovementState { idle, walking }
     private enum Facing { front, back, side }
-    private float dirX, dirY;
+    private List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
     [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float collisionOffset = 0.05f;
 
     private Vector2 movement;
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer sr;
     private BoxCollider2D bc;
-
     private Facing lastFacing = Facing.front, currentFacing;
     private MovementState currentState = MovementState.idle;
+    public ContactFilter2D movementFilter;
+    [SerializeField] private LayerMask barrier;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -30,41 +35,61 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        MovementUpdate();
+        if (IsBehind())
+        {
+            sr.sortingLayerID = SortingLayer.NameToID("Player");
+        }
+        else
+        {
+            sr.sortingLayerID = SortingLayer.NameToID("Default");
+        }
         AnimationStateUpdate();
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + (movement * movementSpeed * Time.fixedDeltaTime));
+        if (movement != Vector2.zero)
+        {
+            bool success = TryMove(movement);
+            if (!success)
+            {
+                success = TryMove(new Vector2(movement.x, 0));
+                if (!success)
+                {
+                    TryMove(new Vector2(0, movement.y));
+                }
+            }
+        }
     }
 
-    private void MovementUpdate()
+    void OnMove(InputValue movementValue)
     {
-        dirX = Input.GetAxisRaw("Horizontal");
-        dirY = Input.GetAxisRaw("Vertical");
+        movement = movementValue.Get<Vector2>();
+    }
 
-        if (dirX != 0f && dirY == 0f)
+    private bool TryMove(Vector2 direction)
+    {   
+        int count = rb.Cast(movement, movementFilter, castCollisions, movementSpeed * Time.fixedDeltaTime + collisionOffset);
+
+        if(count == 0)
         {
-            movement = new Vector2(dirX, 0);
+            rb.MovePosition(rb.position + (movement * movementSpeed * Time.fixedDeltaTime));
+            return true;
         }
-        else if (dirY != 0f && dirX == 0f)
+        else
         {
-            movement = new Vector2(0, dirY);
-        }
-        else if (dirX == 0f && dirY == 0f)
-        {
-            movement = Vector2.zero;
+            return false;
         }
     }
 
     private void AnimationStateUpdate()
     {
         
-        if (movement.x > 0f || movement.x > 0f && Input.GetButtonUp("Vertical"))
+        if (movement.x > 0f)
         {
             currentState = MovementState.walking;
             currentFacing = lastFacing = Facing.side;
+            sr.flipX = false;
         }
         else if (movement.x < 0f)
         {
@@ -90,5 +115,10 @@ public class PlayerMovement : MonoBehaviour
 
         anim.SetInteger("state", (int)currentState);
         anim.SetInteger("facing", (int)currentFacing);
+    }
+
+   private bool IsBehind()
+    {
+        return Physics2D.BoxCast(new Vector2(bc.bounds.center.x, bc.bounds.center.y - 0.375f), new Vector2(bc.bounds.size.x + 0.5f, bc.bounds.size.y + 0.5f), 0f, Vector2.down, .1f, barrier);
     }
 }
